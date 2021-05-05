1d4a1abd6a8cd9e77e58a418573d6d6f4c61958d
serxu <59147261+serxu@users.noreply.github.com>
2021-05-05 13:18:44 +0200

    proyecto creado

---
+++ PracticaMinisterioAgricultura/src/main/java/com/example/demo/PracticaMinisterioAgriculturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaMinisterioAgriculturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaMinisterioAgriculturaApplication.class, args);
+	}
+
+}
