33b587e1f9785c0a054617ae1ccfc266aacee4bd
serxu <59147261+serxu@users.noreply.github.com>
2021-05-05 14:09:33 +0200

    Update MainController.java

--- PracticaMinisterioAgricultura/src/main/java/com/example/demo/MainController.java
+++ PracticaMinisterioAgricultura/src/main/java/com/example/demo/MainController.java
@@ -1,5 +1,28 @@
 package com.example.demo;
 
-public class MainController {
+import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	private ProductoFitosanitarioRepository repProductosFitosanitarios;
+	@Autowired
+	private SustanciaActivaRepository repSustanciasActivas;
+	
+	
+	@PostConstruct
+	public void init() {
+		
+	}
+	
+	@RequestMapping("/")
+	public String controller(Model model) {
+		return "Main.html";
+	}
 }
